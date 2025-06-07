6ab5c6d4df1d47efb2e458b5eaae10218da44378
NashWu <127585626+nashwu0117@users.noreply.github.com>
2025-06-07 16:10:24 +0800

    2025.05.19(Ch1結束)

--- Ch3/ch3_3.py
+++
@@ -1,6 +0,0 @@
-x = 10.5
-print(x)
-print((x))
-y = int(x)+5
-print(y)
-print(type(y))
--- Ch3/ch3_8.py
+++
@@ -1,6 +0,0 @@
-x = 'Deepwisdom means deepen your wisdom'
-print(x)
-print(type(x))
-y = '深智數位  - Deepen your wisdom'
-print(y)
-print(type(y))
